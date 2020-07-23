ea18f8a05d2a2bb1712ba297b69e043f6a289257
strapbool <rafael_rx@hotmail.com>
2020-07-23 10:14:33 -0300

    Change bot for puppeteer

--- index.js
+++ index.js
@@ -1,38 +1,10 @@
 require("dotenv").config();
-const client = require("twilio")(process.env.ACCOUNTSID, process.env.AUTHTOKEN);
-
-function sleep(ms) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
-}
-
-async function send_message(name, number) {
-  await client.messages
-    .create({
-      body: `Oi ${name}, tudo bem?
-
-    Esse é um lembrete automático relacionado à próxima reunião semanal.
-    
-    Segue o formulário: ${process.env.URL}`,
-
-      from: `whatsapp:+${number}`,
-      to: `whatsapp:+${process.env.SENDER}`,
-    })
-    .then((message) => console.log(message.sid))
-    .catch((err) => console.log(err))
-    .done();
-}
-
-async function main() {
-  for (var i = 0; i < process.env.NAMES.split(" ").length; i++) {
-    send_message(
-      process.env.NAMES.split(" ")[i],
-      process.env.NUMBERS.split(" ")[i]
-    );
-
-    await sleep(5000);
-  }
-}
-
-main();
+const puppeteer = require("puppeteer");
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(
+    "https://web.whatsapp.com/send?phone=5511951301230&text=Olá, mundo"
+  );
+  await browser.close();
+})();
